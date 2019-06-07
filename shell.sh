0431ba3356bd0fcf360f22204bbf1359e6df94fc
yimingzenmedi <31919394+yimingzenmedi@users.noreply.github.com>
2019-06-07 13:20:32 +1200

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,5 +1,4 @@
-create a RDS
-
+------create a RDS
 configuration on ubuntu instance(use same security group)
 1. log in
 ssh -i "C:\key\test.pem" ubuntu@ec2-18-217-166-223.us-east-2.compute.amazonaws.com
@@ -11,10 +10,10 @@ mysql -h database-2.caf3kfsfob9t.us-east-2.rds.amazonaws.com -P 3306 -u shi -p
 4. Setting Up MySQL/JDBC Driver on Ubuntu
 sudo apt-get install libmysql-java
 
-configuration for Java enviroment
+------configuration for Java enviroment
 TODO-------------------
 
-Parameter for Database 
+------Parameter for Database 
 String url = "jdbc:mysql://database-2.caf3kfsfob9t.us-east-2.rds.amazonaws.com:3306/";
 String userName = "shi";
 String password = "dafeifei";
@@ -22,7 +21,7 @@ String dbName = "database-2";
 String driver = "com.mysql.jdbc.Driver";
 Connection connection = DriverManager.getConnection(url + dbName, userName, password);
 
------Test JDBC
+------Test JDBC
 import java.sql.*;
 import java.util.Properties;
 
